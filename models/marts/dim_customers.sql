5974e3fbce4d5d365021cbe25392f424cdc837a4
anastasiya_zhukova@discovery.com <anastasiya_zhukova@discovery.com>
2023-05-15 00:35:39 +0000

    added comments to dim_customers.sql and stg_orders.sql

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -11,10 +11,9 @@ select * from {{ ref('stg_orders') }}
 ),
 
 customer_orders as (
-
+-- adding a comment to test CI
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
